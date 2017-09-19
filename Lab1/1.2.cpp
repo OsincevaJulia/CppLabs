f1740f6915f695aa8b090b6b976ddc11fe0c5c3d
Unknown <julia28.1999@gmail.com>
2017-09-19 17:08:26 +0500

    Lab1

---
+++ Lab1/1.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a, b, c;
+	cout << "vvedite storony kyrpicha: ";
+	cin >> a, b, c;
+	double x, y;
+	cout << "vvedite storony otverstiya: ";
+	cin >> x, y;
+	if (a*b < x*y)
+		cout << "Kyrpich proidyot v otverstie.";
+	else
+		if (b*c < x*y)
+			cout << "Kyrpich proidyot v otverstie.";
+		else
+			if (a*c < x*y)
+				cout << "Kyrpich proidyot v otverstie.";
+			else cout << "Kyrpich ne proidyot v otverstie.";
+			return 0;
+}
